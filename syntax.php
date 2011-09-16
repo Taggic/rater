fb0347b723a9ae6fb5ab8403c5cc67db07abe965
Taggic <taggic@t-online.de>
2011-09-16 11:34:57 +0200

    Bugfix on vote thumb down
    Signed-off-by: Taggic <taggic@t-online.de>

--- syntax.php
+++ syntax.php
@@ -230,7 +230,7 @@ class syntax_plugin_rater extends DokuWiki_Syntax_Plugin
                                   
               $ret .= '<TR><TD><a class="thumbup tup" href="doku.php?id='.$ID.'&do=rate_voteup&rater_id='.$rater_id.'&rater_name='.$rater_name.'" /></a>'.
                    '<span id="vote1_1" style="color:#5b8f22">('.$vote1.')&nbsp</span>'.
-                   '<a class="thumbdown tdn" href="doku.php?id='.$ID.'&do=rate_votedown" /></a>'.
+                   '<a class="thumbdown tdn" href="doku.php?id='.$ID.'&do=rate_votedown&rater_id='.$rater_id.'&rater_name='.$rater_name.'" /></a>'.
                    '<span id="vote1_2" style="color:#FF1822">('.$vote2.')</span></TD></TR>';
               if($rater_msg!="") $ret .= "<div>".$rater_msg."</div>";
               $ret .= '</form>';
